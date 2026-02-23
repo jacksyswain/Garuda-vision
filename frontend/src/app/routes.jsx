7820280013fff1e3711c501c08e24b9314d80477
jyotiprakash swain <jp.swainabc@gmail.com>
2026-02-23 22:19:10 +0530

    new page added

--- frontend/src/app/routes.jsx
+++ frontend/src/app/routes.jsx
@@ -1,20 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
+import CameraDetails from "../pages/CameraDetails";
 import ProtectedRoute from "../components/layout/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* Protected Dashboard */}
         <Route
           path="/"
           element={
             <ProtectedRoute>
               <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+       
+        <Route
+          path="/camera/:id"
+          element={
+            <ProtectedRoute>
+              <CameraDetails />
             </ProtectedRoute>
           }
         />
