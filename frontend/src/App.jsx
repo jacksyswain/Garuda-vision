6348ec77ff18b9e6a969f9ce5d5d03e5007b7751
jyotiprakash swain <jp.swainabc@gmail.com>
2026-02-26 21:28:16 +0530

    new auth logic added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,7 @@ import { AuthProvider } from "./context/AuthContext";
 function App() {
   return (
     <AuthProvider>
-       <div className="bg-blue-600 text-white text-4xl p-10">
-      Tailwind Is Working
-    </div>
+       
       <AppRoutes />
     </AuthProvider>
   );
